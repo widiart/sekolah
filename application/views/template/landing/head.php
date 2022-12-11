412e0e273e5f44676fd3a733dbfafe0dabb90803
widiart <widiartoputra@gmail.com>
2022-12-11 16:04:47 +0700

    update favicon

--- application/views/template/landing/head.php
+++ application/views/template/landing/head.php
@@ -7,7 +7,7 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="<?= base_url("assets/img/favicon.png") ?>" rel="icon">
+  <link href="<?= base_url("assets/img/logo.jpg") ?>" rel="icon">
   <link href="<?= base_url("assets/img/apple-touch-icon.png") ?>" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
